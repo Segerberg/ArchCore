0d39a55144bd882afbc840cc9c2ba08ec9ae041b
segerberg <andreassegerberg@gmail.com>
2024-10-19 13:48:16 +0200

    WIP

--- app/data_management/routes.py
+++ app/data_management/routes.py
@@ -1,6 +1,6 @@
 import os
 
-from flask import render_template, request, url_for, redirect
+from flask import render_template, request, url_for, redirect, jsonify
 from flask_login import login_required
 from app.data_management import bp
 from app.models import Node
@@ -43,7 +43,7 @@ def archival_descriptions():
     prev_url = url_for('data_management.archival_descriptions', page=nodes.prev_num) \
         if nodes.has_prev else None
 
-    return render_template('data_management/archival_descriptions/index.jinja2',
+    return render_template('data_management/ad2/index.jinja2',
                            nodes=nodes,
                            next_url=next_url,
                            prev_url=prev_url)
@@ -52,11 +52,27 @@ def archival_descriptions():
 @bp.route('/data-management/archival-descriptions/<id>', methods=['GET', 'POST'])
 @login_required
 def archival_descriptions_detail(id, reload=False):
-    node = Node.query.get(id)
-    if request.method == "POST":
-        return render_template('data_management/archival_descriptions/node_detail.jinja2', node=node)
+    node = Node.query.get_or_404(id)
+    first_level_children = node.children.all()  # Preload first-level children
+    #if request.method == "POST":
+    #    return render_template('data_management/archival_descriptions/node_detail.jinja2', node=node)
 
-    return render_template('data_management/archival_descriptions/node.jinja2', node=node, reload=reload)
+    #return render_template('data_management/archival_descriptions/node.jinja2', node=node, reload=reload)
+    return render_template('data_management/ad2/tree.jinja2', node=node, reload=reload)
+
+
+
+@bp.route('/node/<int:node_id>/children', methods=['GET'])
+def get_children(node_id):
+    node = Node.query.get_or_404(node_id)
+    children = node.children.all()
+    return render_template('data_management/ad2/_children.jinja2', children=children)
+
+
+@bp.route('/node/<int:node_id>/details', methods=['GET'])
+def get_node_details(node_id):
+    node = Node.query.get_or_404(node_id)
+    return render_template('data_management/ad2/node_detail.jinja2', node=node)
 
 
 @bp.route('/data-management/archival-descriptions/node/<id>', methods=['GET', 'POST'])
@@ -69,32 +85,34 @@ def get_node(id):
 
     return render_template('data_management/archival_descriptions/_tree_node.jinja2', node=node)
 
-@bp.route('/data-management/archival-descriptions/node/edit/<id>', methods=['GET','POST'])
-@login_required
-def edit_node(id):
-    node = Node.query.get(id)
+
+@bp.route('/node/<int:node_id>/edit', methods=['GET', 'POST'])
+def edit_node(node_id):
+    node = Node.query.get_or_404(node_id)
     lod_list = os.getenv('LEVELS_OF_DESCRIPTION').split(',')
 
     if request.method == 'POST':
         node.title = request.form['title']
         if request.form['date_start']:
-            date_start = datetime.strptime(request.form['date_start'], "%Y-%m-%d").date()
-            node.date_start = date_start
+            node.date_start = datetime.strptime(request.form['date_start'], "%Y-%m-%d").date()
         if request.form['date_end']:
-            date_end = datetime.strptime(request.form['date_end'], "%Y-%m-%d").date()
-            node.date_end = date_end
+            node.date_end = datetime.strptime(request.form['date_end'], "%Y-%m-%d").date()
         node.archival_history = request.form['archival_history']
         node.level_of_description = request.form['level_of_description']
+
         db.session.commit()
-        return render_template('data_management/archival_descriptions/node.jinja2', node=node)
 
-    return render_template('data_management/archival_descriptions/edit_node.jinja2', node=node, lod_list=lod_list)
+        # Return the updated node details after saving
+        return render_template('data_management/ad2/node_detail.jinja2', node=node)
+
+    # Render the edit form
+    return render_template('data_management/ad2/_edit_node.jinja2', node=node, lod_list=lod_list)
 
 
-@bp.route('/data-management/archival-descriptions/node/add', defaults={'parent_id': None}, methods=['GET', 'POST'])
-@bp.route('/data-management/archival-descriptions/node/add/<parent_id>', methods=['GET', 'POST'])
+@bp.route('/node/<int:parent_id>/add_child', methods=['GET', 'POST'])
+@bp.route('/node/add_root', methods=['GET', 'POST'])
 @login_required
-def add_node(parent_id):
+def add_node(parent_id=None):
     lod_list = os.getenv('LEVELS_OF_DESCRIPTION', '').split(',')
     parent_node = Node.query.get(parent_id) if parent_id else None
 
@@ -113,9 +131,17 @@ def add_node(parent_id):
         node.parent = parent_node
         db.session.add(node)
         db.session.commit()
-        return render_template('data_management/archival_descriptions/node.jinja2', node=node)
+        if parent_node:
+            return render_template('data_management/ad2/node_detail.jinja2', node=node)
+        else:
+            response = jsonify(success=True)  # Placeholder response
+            response.headers['HX-Redirect'] = url_for('data_management.archival_descriptions_detail', id=node.id)
+            return response
     else:
-        return render_template('data_management/archival_descriptions/add_node.jinja2', node=parent_node, parent_node=parent_node, lod_list=lod_list)
+        if parent_node:
+            return render_template('data_management/ad2/_add_node.jinja2', node=parent_node, parent_node=parent_node, lod_list=lod_list)
+        else:
+            return render_template('data_management/ad2/_add_root_node.jinja2', lod_list=lod_list)
 
 
 
